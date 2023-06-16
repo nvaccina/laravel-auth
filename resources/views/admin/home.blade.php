954f70f11b70c163b3933f98c62af691f8202192
nvaccina <niccolovac@gmail.com>
2023-06-16 14:24:47 +0200

    Create admin.partials.header & aside

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,26 +1,15 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
-<div class="container">
+<div class="container p-5">
     <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
+        Home Dashboard
     </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus temporibus ratione fugit vitae officiis, quia cumque a, minima aut quasi tenetur veritatis fugiat praesentium ad amet eos ducimus, perspiciatis cupiditate qui dolor explicabo dignissimos alias voluptate dolore. Quaerat corrupti provident ipsam libero illum. Quis eos ad consequatur veniam repudiandae molestiae molestias repellat, eveniet dolores reiciendis deleniti cum natus error, voluptas obcaecati blanditiis adipisci optio nesciunt! Praesentium, odit! Reiciendis, doloremque? Hic, consequuntur eligendi, porro esse sunt quidem iusto dignissimos maxime, perferendis saepe at illo enim ullam quaerat ipsum explicabo illum in vitae aliquid ducimus odio ut soluta pariatur vero? Esse vitae praesentium eveniet, blanditiis expedita labore culpa quo quia? Blanditiis expedita in praesentium? Alias deserunt temporibus quaerat animi quia nulla error numquam perferendis? Et quidem saepe voluptate ab, labore, maiores aliquam quae fuga illo dicta esse eveniet corrupti quasi iusto iure mollitia veniam rerum exercitationem sequi ea? Ducimus et voluptatem velit maiores reprehenderit in provident eius dolore sed, maxime a beatae accusamus unde quae natus, cupiditate odit ex, ipsa laudantium numquam dignissimos earum! Omnis neque repellat itaque, voluptatibus magni beatae eius vero! Odio id fugiat facilis sit praesentium nisi earum accusamus deleniti corporis error nulla illo dolores, debitis at eum voluptate!
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus temporibus ratione fugit vitae officiis, quia cumque a, minima aut quasi tenetur veritatis fugiat praesentium ad amet eos ducimus, perspiciatis cupiditate qui dolor explicabo dignissimos alias voluptate dolore. Quaerat corrupti provident ipsam libero illum. Quis eos ad consequatur veniam repudiandae molestiae molestias repellat, eveniet dolores reiciendis deleniti cum natus error, voluptas obcaecati blanditiis adipisci optio nesciunt! Praesentium, odit! Reiciendis, doloremque? Hic, consequuntur eligendi, porro esse sunt quidem iusto dignissimos maxime, perferendis saepe at illo enim ullam quaerat ipsum explicabo illum in vitae aliquid ducimus odio ut soluta pariatur vero? Esse vitae praesentium eveniet, blanditiis expedita labore culpa quo quia? Blanditiis expedita in praesentium? Alias deserunt temporibus quaerat animi quia nulla error numquam perferendis? Et quidem saepe voluptate ab, labore, maiores aliquam quae fuga illo dicta esse eveniet corrupti quasi iusto iure mollitia veniam rerum exercitationem sequi ea? Ducimus et voluptatem velit maiores reprehenderit in provident eius dolore sed, maxime a beatae accusamus unde quae natus, cupiditate odit ex, ipsa laudantium numquam dignissimos earum! Omnis neque repellat itaque, voluptatibus magni beatae eius vero! Odio id fugiat facilis sit praesentium nisi earum accusamus deleniti corporis error nulla illo dolores, debitis at eum voluptate!
+    </p>
 </div>
 @endsection
---
+++ resources/views/admin/partials/aside.blade.php
@@ -0,0 +1,24 @@
+<aside class="bg-dark py-4">
+    <ul>
+        <li class="py-2">
+            <a href="{{route('admin.home')}}">
+                <i class="pe-2 fa-solid fa-chart-line"></i>Dashboard
+            </a>
+        </li>
+        <li class="py-2">
+            <a href="#">
+                <i class="pe-2 fa-regular fa-folder"></i>Portfolio
+            </a>
+        </li>
+        <li class="py-2">
+            <a href="#">
+                <i class="pe-2 fa-regular fa-square-plus"></i>New Portfolio
+            </a>
+        </li>
+        <li class="py-2">
+            <a href="#">
+                <i class="pe-2 fa-regular fa-square-plus"></i> New Work
+            </a>
+        </li>
+    </ul>
+</aside>
